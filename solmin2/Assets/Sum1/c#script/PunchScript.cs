3af9fe5c61771925fc01395ddd317513333993bd
solming9 <leesm0404a@naver.com>
2022-10-10 22:57:52 +0900

    ok

---
+++ solmin2/Assets/Sum1/c#script/PunchScript.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchScript : MonoBehaviour
+{
+    public int[] attack = new int[3];
+    int i;
+
+    public int num;
+    public int sign;
+    public int result;
+
+    int first;
+
+    public GameObject NumScollview; //숫자
+    public GameObject CalculScollview; //연산기호
+
+    public void re () //계산 초기화 함수
+    {
+        i = 0;
+        sign = 0;
+        result = 0;
+        first = 1;
+        num = -1;
+    }
+
+    void Start () //게임 시작 초기화
+    {
+        num = -1;
+    }
+
+    void Update() //계산 초기화 실행 함수
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            re();
+            ScrollChange2();
+        }
+    }
+
+    public void calculator () //계산 함수
+    {
+        if(i == 0) //첫번째 숫자
+        {
+            attack[i] = num;
+            i++;
+            result = num;
+            ScrollChange1();
+        }
+        else if(i == 1) //두번째 연산기호
+        {
+            attack[i] = sign;
+            i++;
+            ScrollChange2();
+        }
+        else if(i == 2) //세번째 숫자
+        {
+            attack[i] = num;
+            ScrollChange1();
+            if (attack[1] == 1 && first == 1)
+            {
+                result = attack[0] + attack[2];
+                first = 0;
+            }
+            else if (attack[1] == 2 && first == 1)
+            {
+                result = attack[0] - attack[2];
+                first = 0;
+            }
+            else if (attack[1] == 3 && first == 1)
+            {
+                result = attack[0] * attack[2];
+                first = 0;
+            }
+            else if (attack[1] == 4 && first == 1)
+            {
+                result = attack[0] / attack[2];
+                first = 0;
+            }
+            else if (attack[1] == 1 && first == 0)
+            {
+                result += attack[2];
+            }
+            else if (attack[1] == 2 && first == 0)
+            {
+                result -= attack[2];
+            }
+            else if (attack[1] == 3 && first == 0)
+            {
+                result *= attack[2];
+            }
+            else if (attack[1] == 4 && first == 0)
+            {
+                result /= attack[2];
+            }
+            i = 1;
+        }
+    }
+
+    public void ScrollChange1() //연산기호만
+    {
+        NumScollview.SetActive(false);
+        CalculScollview.SetActive(true);
+    }
+    public void ScrollChange2() //숫자만
+    {
+        NumScollview.SetActive(true);
+        CalculScollview.SetActive(false);
+    }
+}
---
+++ solmin2/Assets/Sum1/c#script/Stage.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    private int stage; //현재 단계 확인용 변수
+    private bool stagemove; //단계 사이 이동 시간 확보용 변수
+
+    public int remain; //단계별 남은 몬스터 수 확인용 변수
+
+    public GameObject cul; //플레이어 머리 위 무기
+
+    public GameObject Stage1Moster; //단계별 몬스터 소환용
+    public GameObject Stage2Moster;
+    public GameObject Stage3Moster;
+
+    public GameObject punch;
+    public GameObject NumScollview; //숫자
+    public GameObject CalculScollview; //연산기호
+
+    GameObject ForDestroy;
+
+    public void Start() //게임 시작 초기화
+    {
+        NumScollview.SetActive(true);
+        CalculScollview.SetActive(false);
+
+        punch.GetComponent<PunchScript>().re();
+
+        stage = 1;
+        stagemove =false;
+        remain = 3;
+    }
+
+    void Update()
+    {
+        if(stage == 1 && stagemove == false) //1단계 시작
+        {
+            Debug.Log("1단계 시작");
+            cul.SetActive(true);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Instantiate(Stage1Moster, new Vector3(12 + i, 3, 0), Stage1Moster.transform.rotation);
+            }
+            stagemove = true;
+        }
+
+        if (stage == 1 && remain == 0) //1단계 종료
+        {
+            Debug.Log("1단계 클리어");
+
+            cul.SetActive(false);
+            GameObject.Find("Player").GetComponent<PlayerScript>().move = 1;
+            Invoke("StageMove", 11f);
+
+            remain = 3;
+        }
+
+        if (stage == 2 && stagemove == false) //2단계 시작
+        {
+            Debug.Log("2단계 시작");
+            cul.SetActive(true);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Instantiate(Stage2Moster, new Vector3(12 + i, 3, 0), Stage2Moster.transform.rotation);
+            }
+            stagemove = true;
+        }
+
+        if (stage == 2 && remain == 0) //2단계 종료
+        {
+            Debug.Log("2단계 클리어");
+
+            cul.SetActive(false);
+            GameObject.Find("Player").GetComponent<PlayerScript>().move = 1;
+            Invoke("StageMove", 11f);
+
+            remain = 3;
+        }
+
+        if (stage == 3 && stagemove == false) //3단계 시작
+        {
+            Debug.Log("3단계 시작");
+            cul.SetActive(true);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Instantiate(Stage3Moster, new Vector3(12 + i, 3, 0), Stage3Moster.transform.rotation);
+            }
+            stagemove = true;
+        }
+
+        if (stage == 3 && remain == 0) //클리어
+        {
+            Debug.Log("클리어!");
+            cul.SetActive(false);
+            Invoke("StageEnding", 2f);
+            Invoke("Clear", 5f);
+
+            remain = 3; //얘는 남은 몬스터 수 확인용이 아니라 한 번만 실행되도록 하기 위함
+        }
+    }
+
+    void StageMove() //단계 사이 이동 시간 확보용 함수
+    {
+        stage++;
+        stagemove = false;
+    }
+
+    public void StageEnding()
+    {
+        var ending = GameObject.Find("ending").GetComponent<endingscene>();
+        if(ending != null)
+        {
+            ending.endingStart();
+        }
+    }
+
+    public void Clear() //클리어 함수
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void Fail() //실패 함수
+    {
+        Debug.Log("Fail");
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
