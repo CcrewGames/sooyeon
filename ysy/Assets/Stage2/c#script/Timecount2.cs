29ba57296efb26a0c105b4b5bf19fb733b99f169
iioese <iioese109@gmail.com>
2022-11-16 00:20:20 +0900

    11/15

---
+++ ysy/Assets/Stage2/c#script/Timecount2.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timecount2 : MonoBehaviour
+{
+    public float countdownSeconds = 210;
+    private TextMeshProUGUI timeText;
+
+    private bool timeend;
+
+    public GameObject stage;
+
+    private void Start()
+    {
+        timeText = GetComponent<TextMeshProUGUI>();
+        timeend = true;
+    }
+
+    void Update()
+    {
+        if(stage.GetComponent<Stage2>().fortime == 1)
+            countdownSeconds -= Time.deltaTime;
+
+        var span = new TimeSpan(0, 0, (int)countdownSeconds);
+        timeText.text = span.ToString(@"mm\:ss");
+        if (timeend == true){
+            if (countdownSeconds <= 0) //시간 초과 fail
+            {
+                Invoke("Stagetimeout", 1f);
+                Invoke("End", 5f);
+                timeend = false;
+            }
+        }
+    }
+    void Stagetimeout()
+    {
+        var ending = GameObject.Find("ending").GetComponent<endingscene>();
+        if(ending != null)
+        {
+            ending.Stagetimeout();
+        }
+    }
+
+    void End()
+    {
+        //다른 씬으로 넘어가도록 해야 됨
+    }
+}
